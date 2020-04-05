47a9eec1dd10bf6d1f1f8f2094628fad0f475fd5
Alex Posh <oleg.poshiv@gmail.com>
2020-04-04 23:16:32 -0700

    Got the search button to work

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
+    function citySearch (city) {
     //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-    let city = "shanghai";
+    
     let key = "bf81dc4410bae4c75c0172966d86af60";
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
 
@@ -15,11 +16,19 @@ $(document).ready(function () {
         let currentTemp = $("<p>").text("Temperature: " + data.main.temp + " F");
         let currentHumid = $("<p>").text("Humidity: " + data.main.humidity+ "%");
         let currentWindSpeed = $("<p>").text("Wind Speed: " + data.wind.speed +" MPH");
-
+        $("#current").empty();
         $("#current").append(currentTemp,currentHumid, currentWindSpeed);
     });
+};
 
-    
+    $(".btn-primary").on("click", function(event) {
+
+        event.preventDefault();
+
+        let city = $("#userInput").val();
+
+        citySearch(city);
+    });
 
 
 });
