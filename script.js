23c4542600fc6195ebbf4cf2440323522b1ffff0
Alex Posh <oleg.poshiv@gmail.com>
2020-04-06 14:16:49 -0700

    the history now pushes down and saves to storage

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+    let historyArray = [];
+    retrieveHistory();
+
     function citySearch(city) {
         //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
@@ -17,11 +20,11 @@ $(document).ready(function () {
             let currentTemp = $("<p>").text("Temperature: " + data.main.temp + " F");
             let currentHumid = $("<p>").text("Humidity: " + data.main.humidity + "%");
             let currentWindSpeed = $("<p>").text("Wind Speed: " + data.wind.speed + " MPH");
-            let icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon +"@2x.png"); 
+            let icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
             icon.attr("style", "max-width: 70px");
             $("#current").empty();
             $("#current").append(currentTemp, currentHumid, currentWindSpeed, icon);
-            
+
         });
 
         $.ajax({
@@ -29,69 +32,94 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (data) {
 
-            
+
             console.log(data);
             for (let i = 5, j = 0; i < 40; i += 8, j++) {
                 let classes = [".one", ".two", ".three", ".four", ".five"];
                 let myDate = new Date(data.list[i].dt * 1000);
                 let dateString = (myDate.getMonth() + 1) + "/" + (myDate.getDate()) + "/" + myDate.getFullYear();
 
-                let temp = $("<p>").text(data.list[i].main.temp);
+                let temp = $("<p>").text(data.list[i].main.temp + "° F");
                 let date = $("<p>").text(dateString);
                 let humid = $("<p>").text("Humidity: " + data.list[i].main.humidity + "%");
-                
+
                 $(classes[j]).empty();
                 $(classes[j]).append(temp, date, humid);
 
 
             }
 
-
-
         });
 
     };
 
-
-    // function renderStufff() {
-
-    //     // city, 
-    //     // {
-    //     //     cities: [city1, city2, city3]
-    //     // }
-    //     // myStringVal = localStorage.get("city");
-    //     // cityObj = jSONParse(myStringVal)
-    //     // myArray = cityObj.cities
-        
-
-    //     // Get city from localStorage
-
-    //     // For each value in city.cities do stuff
-
-        
-    // }
+    
 
     $(".btn-primary").on("click", function (event) {
 
         event.preventDefault();
 
-        // Add $("#userInput").val() to local storage
-        // Create a new html elment with $("#userInput").val()
-        // add an event handler for the new element
+        let inputText = $("#userInput").val()
+        let city = inputText;
+        // return from function if submitted blank
+        if (city === "") {
+            return;
+        }
 
-        let city = $("#userInput").val();
-
+        historyArray.push(city);
         citySearch(city);
+        inputText = "";
+
+        storeHistory();
+        renderHistory();
     });
 
+    function renderHistory() {
+        // myStringVal = localStorage.get("city");
+        // cityObj = jSONParse(myStringVal)
+        // myArray = cityObj.cities
+        // Get city from localStorage
+        // For each value in city.cities do stuff
+        
+        $("#history").innerHTML = "";
+        
+        //render a new li for each array
+        for (let i = 0; i < historyArray.length; i++) {
+            //store value of array
+            let history = historyArray[i];
+            let li = $("<li>");
+            // add input to the li
+            li.text(history);
+            //place the li into the ul 
+            $("#history").append(li);
+        }
+        
+        
+    };
     // render function
     // checks for city array in local storage
     // json.parse city array
     // for each city in array add a html element in history
-    
     // For each of your history html elements add an event handler to run citySearch(city)
 
+    function storeHistory() {
+        // store the history of cities into an array
+        localStorage.setItem("cities", JSON.stringify(historyArray));
+    };
 
-    
+    function retrieveHistory() {
+
+
+        let storedCities = JSON.parse(localStorage.getItem("cities"));
+        if(storedCities !== null) {
+            // If m retrieved, replace with the new array
+            historyArray = storedCities;
+        }
+
+        renderHistory();
+    };
+
+
+
 
 });
